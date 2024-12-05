1585c335f981bb4fb54f96dec0ec52fe42e40b96
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-12-05 19:40:14 +0200

    Refactor code

--- financeDashboard/src/components/Header.jsx
+++ financeDashboard/src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header(){
     return (
         <div className="header flex justify-between items-center px-10 py-5">
             <div className="left">
-                <div className="font-inter logo border border-black px-4 py-2 rounded-2xl transition ease-in-out delay-200 hover:bg-black hover:text-white hover:scale-110 cursor-pointer">Finaus</div>
+                <div className="font-inter logo border border-black px-4 py-2 rounded-2xl transition ease-in-out delay-200 hover:bg-black hover:text-white hover:scale-110 cursor-pointer text-4xl">Finaus</div>
             </div>
             <div className="right flex items-center gap-10">
                 <div className="links flex border border-black rounded-full  bg-white p-1 gap-2">
