c821a7a63a056c3eeb7bd7faac776cccb0f3c400
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-12-05 11:52:26 +0200

    Style link container for header

--- financeDashboard/src/components/Header.jsx
+++ financeDashboard/src/components/Header.jsx
@@ -1,29 +1,22 @@
+import { Link } from "./Link.jsx"
 export function Header(){
     return (
-        <div className="header flex justify-between px-10 py-5">
+        <div className="header flex justify-between items-center px-10 py-5">
             <div className="left">
                 <div className="font-inter logo border border-black px-4 py-2 rounded-2xl transition ease-in-out delay-200 hover:bg-black hover:text-white hover:scale-110 cursor-pointer">Finaus</div>
             </div>
-            <div className="right">
-                <ul className="links">
-                    <a href="">
-                        <li className="">Dashboard</li>
-                    </a><a href="">
-                        <li className="">People </li>
-                    </a><a href="">
-                        <li className="">Hiring </li>
-                    </a><a href="">
-                        <li className="">Devices </li>
-                    </a><a href="">
-                        <li>App </li>
-                    </a><a href="">
-                        <li>Salary </li>
-                    </a><a href="">
-                        <li>Calender </li>
-                    </a><a href="">
-                        <li>Reviews</li>
-                    </a>
-                </ul>   
+            <div className="right flex items-center gap-10">
+                <div className="links flex border border-black rounded-full  bg-white p-1 gap-2">
+                    <Link text={'Dashboard'}/>
+                    <Link text={'People'}/>
+                    <Link text={'Hiring'}/>
+                    <Link text={'Devices'}/>
+                    <Link text={'App'}/>
+                    <Link text={'Salary'}/>
+                    <Link text={'Calender'}/>
+                    <Link text={'Reviews'}/>
+            
+                </div>
                 <div className="settings">
                     {/* <img src="" alt="" srcset="" />     */}
                     <div className="setting">Settings</div>
---
+++ financeDashboard/src/components/Link.jsx
@@ -0,0 +1,34 @@
+export function Link(props){
+    return (
+        <div className="cursor-pointer flex justify-center items-center rounded-3xl p-2 transition delay-150 ease-in-out hover:bg-black hover:text-white min-h-12 min-w-10">
+            {props.text}
+        </div>
+    )
+}
+
+                {/* <ul className="links flex gap-7 border border-black rounded-full px-4 py-4 bg-white">
+                    <a href="">
+                        <li className="hover:bg-black min-h-4 min-w-4">Dashboard</li>
+                    </a>
+                    <a href="">
+                        <li className="">People </li>
+                    </a>
+                    <a href="">
+                        <li className="">Hiring </li>
+                    </a>
+                    <a href="">
+                        <li className="">Devices </li>
+                    </a>
+                    <a href="">
+                        <li>App </li>
+                    </a>
+                    <a href="">
+                        <li>Salary </li>
+                    </a>
+                    <a href="">
+                        <li>Calender </li>
+                    </a>
+                    <a href="">
+                        <li>Reviews</li>
+                    </a>
+                </ul>    */}
