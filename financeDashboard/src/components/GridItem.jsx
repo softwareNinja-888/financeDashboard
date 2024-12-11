1437cf72c6f94925c94ea9c531682557c5321b72
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-12-11 12:32:10 +0200

    Make pie chart in insight smaller

--- financeDashboard/src/components/GridItem.jsx
+++ financeDashboard/src/components/GridItem.jsx
@@ -1,6 +1,6 @@
 export function Item(props){
     return (
-        <div className={`${props.space} border border-red-500 min-w-10 min-h-64 rounded-3xl`}>
+        <div className={`${props.space} border border-red-500 min-w-10 min-h-64 rounded-3xl `}>
             {props.child ? <props.child/>: null}
         </div>
     )
--- financeDashboard/src/components/Insight.jsx
+++ financeDashboard/src/components/Insight.jsx
@@ -6,15 +6,23 @@ import { SegmentCircle } from "./Charts.jsx";
 export function Insight(){
 
     const data = [
-        { name: "Group A", value: 400, fill: "#ffc658" },
+        { name: "Group A", value: 1400, fill: "#ffc658" },
         { name: "Group B", value: 300, fill: "#8884d8" },
         { name: "Group C", value: 300, fill: "#82ca9d" },
         { name: "Group D", value: 200, fill: "#ff8042" }
     ];
 
     return (
-            <div className="h-full rounded-3xl bg-white cursor-pointer">
+            <div className="flex flex-col items-center h-full rounded-3xl bg-white cursor-pointer">
+                <div className="insightHead">
+                    <span className="">All Expenses</span>
+                </div>
                 <SegmentCircle dataNew={data}/>
+                <div className="cat">
+                    <span className=""></span>
+                    <span className=""></span>
+                    <span className=""></span>
+                </div>
             </div>
         )
 
